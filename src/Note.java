2524429fdce28afa289afd158a67dc2787efcf03
Adil Ahmad <aahmad8@luc.edu>
2017-10-08 15:09:11 +0000

    Made changes to Note.java class specifically for constructor

--- src/Note.java
+++ src/Note.java
@@ -8,28 +8,28 @@ import java.util.regex.*;
  *  Note class
  */
 public class Note {
- 
-  private String name;
-  private String path;
-  private File file;
-  private ArrayList<String> mentions;
-  private ArrayList<String> keywords;
-  private String identifier;
-  private ArrayList<String> links;
-  private ArrayList<String> urls;
-  
-  // Constructor
-  public Note(File file) {
-    mentions = new ArrayList<String>();
-    keywords = new ArrayList<String>();
-    identifier = null;
-    links = new ArrayList<String>();
-    urls = new ArrayList<String>();
-    this.name = file.getName();
-    this.path = file.getPath();
-    this.file = file;
-    parse();
-  }
+    
+    private String name;
+    private String path;
+    private File file;
+    private ArrayList<String> mentions;
+    private ArrayList<String> keywords;
+    private String identifier;
+    private ArrayList<String> links;
+    private int length;
+    
+    // Constructor
+    public Note(File file) {
+        mentions = new ArrayList<String>();
+        keywords = new ArrayList<String>();
+        identifier = null;
+        links = new ArrayList<String>();
+        this.name = file.getName();
+        this.path = file.getPath();
+        this.file = file;
+        this.length = 0;
+        parse();
+    } 
   
   // Method to parse Note file
   private void parse() {
